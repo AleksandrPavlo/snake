0a7a8c944e753ab1c7ab68caaf4f25138b748d9c
AleksandrPavlo <pplqiwi@gmaol.com>
2016-07-06 17:42:46 +0300

    змейка

---
+++ snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1,3,'*');
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+            Console.ReadLine();
+        }
+      
+    }
+}
